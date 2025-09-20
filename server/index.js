8015eef8aeb87414ba0418031309efe609f87fd1
JAIKANNA.B <117563781+jz6102@users.noreply.github.com>
2025-09-20 13:01:14 +0530

    fix: Configure CORS for production

--- server/index.js
+++ server/index.js
@@ -15,7 +15,9 @@ const app = express();
 
 // Basic middlewares
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: "attack-capital-openmic-client-bbq6qz727-jaikanna-bs-projects.vercel.app" 
+}));
 app.use(express.json({ limit: '1mb' }));
 app.use(morgan('dev'));
 
